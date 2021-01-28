8564e8f926a22e4bc346ee5d993842e58901361d
infelipe13 <felipe.oliveira@escolamais.com>
2021-01-28 17:49:58 -0300

    Create endpoints to serve map and tileset JSON files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 
+import map from "src/assets/maps/map.json";
+import tileset from "src/assets/tilesets/tileset.json";
+
 const PORT = Number(process.env.PORT) || 5000;
 
 const app = express();
 
-app.get("/", (_, res) => {
-	res.send("Hello, world!!!");
+app.get("/map", (_, res) => {
+	res.send(map);
+});
+
+app.get("/tileset", (_, res) => {
+	res.send(tileset);
 });
 
 app.listen(PORT, () => {

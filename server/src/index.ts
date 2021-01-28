25869145cc7572d160119ce834d57fb3f0ab6e80
infelipe13 <felipe.oliveira@escolamais.com>
2021-01-28 13:33:55 -0300

    Setup server

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("Hello, world!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
+});
